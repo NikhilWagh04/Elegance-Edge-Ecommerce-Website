8db8ba8c9b9e34e208780bf56eeb7c0077def6bc
Vijay <vkumar80090@gmail.com>
2024-08-06 22:30:32 +0530

    validation

--- src/redux/apiCalls.js
+++ src/redux/apiCalls.js
@@ -1,10 +1,9 @@
 import { loginFailure, loginStart, loginSuccess } from "./userRedux";
 import { publicRequest } from "../requestMethods";
 import axios from "axios";
-import { toast } from "react-toastify";
-import 'react-toastify/dist/ReactToastify.css';
+import { toast, ToastContainer } from 'react-toastify';
 // Initialize react-toastify
-toast.configure();
+
 export const login = async (dispatch, user) => {
     console.log(user)
   dispatch(loginStart());
@@ -13,8 +12,9 @@ export const login = async (dispatch, user) => {
     console.log(res.data);
     localStorage.setItem("userId", res.data._id);
     localStorage.setItem(`cart_${res.data._id}`, JSON.stringify(res.data.cart));
-    dispatch(loginSuccess(res.data));
     toast.success("Login successful!");
+    dispatch(loginSuccess(res.data));
+   
   } catch (err) {
     dispatch(loginFailure());
     toast.error(err);
